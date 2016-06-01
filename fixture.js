33c8097d71488e446a247f72ce77ebf03fd28d98
Liu Yang <zation1@gmail.com>
2016-06-01 22:14:30 +0800

    basic function

--- fixture.js
+++ fixture.js
@@ -1,17 +1,8 @@
 import { defineMessages } from 'react-intl';
 
 defineMessages({
-  checkbox: {
-    id: 'portfolio.risk.scenario.checkbox',
-    defaultMessage: '情景模拟',
-  },
-  select: {
-    id: 'portfolio.risk.scenario.select',
-    defaultMessage: '请选择情景',
-  },
-  button: {
-    id: 'portfolio.risk.scenario.button',
-    defaultMessage: '开始模拟',
-  },
+  checkbox: '情景模拟',
+  select: '请选择情景',
+  button: '开始模拟',
 });
 
--- src/index.js
+++ src/index.js
@@ -1,200 +1,45 @@
 import p from 'path';
 import { writeFileSync } from 'fs';
 import { sync as mkdirpSync } from 'mkdirp';
-import printICUMessage from './print-icu-message';
 
 const FUNCTION_NAMES = [
   'defineMessages',
 ];
 
-const DESCRIPTOR_PROPS = new Set(['id', 'description', 'defaultMessage']);
+const convertToObject = objectExpression => objectExpression.node.properties.reduce(
+  (object, { key, value }) => ({ ...object, [key.name]: value.value }),
+  {}
+);
 
-export default function() {
-  function getModuleSourceName(opts) {
-    return opts.moduleSourceName || 'react-intl';
-  }
+export default () => ({
+  visitor: {
+    CallExpression: (path, state) => {
+      const { opts } = state;
+      const moduleSourceName = opts.moduleSourceName || 'react-intl';
+      const callee = path.get('callee');
 
-  function getMessageDescriptorKey(path) {
-    if (path.isIdentifier() || path.isJSXIdentifier()) {
-      return path.node.name;
-    }
+      if (callee.isIdentifier() &&
+        FUNCTION_NAMES.some(name => callee.referencesImport(moduleSourceName, name))) {
 
-    let evaluated = path.evaluate();
-    if (evaluated.confident) {
-      return evaluated.value;
-    }
+        const { file: { opts: { basename, filename } } } = state;
+        const relativePath = p.relative(process.cwd(), filename);
+        const dir = p.join(opts.messagesDir, p.dirname(relativePath));
+        const filePath = p.format({
+          dir,
+          ext: '.json',
+          base: `${basename}.json`,
+          name: basename,
+        });
 
-    throw path.buildCodeFrameError(
-      '[React Intl] Messages must be statically evaluate-able for extraction.'
-    );
-  }
+        const messagesObject = convertToObject(path.get('arguments')[0]);
+        const messages = Object.keys(messagesObject).map(key => ({
+          id: `${relativePath}-${key}`,
+          defaultMessage: messagesObject[key],
+        }));
 
-  function getMessageDescriptorValue(path) {
-    if (path.isJSXExpressionContainer()) {
-      path = path.get('expression');
-    }
-
-    let evaluated = path.evaluate();
-    if (evaluated.confident) {
-      return evaluated.value;
-    }
-
-    throw path.buildCodeFrameError(
-      '[React Intl] Messages must be statically evaluate-able for extraction.'
-    );
-  }
-
-  function createMessageDescriptor(propPaths) {
-    return propPaths.reduce((hash, [keyPath, valuePath]) => {
-      let key = getMessageDescriptorKey(keyPath);
-
-      if (!DESCRIPTOR_PROPS.has(key)) {
-        return hash;
+        mkdirpSync(dir);
+        writeFileSync(filePath, JSON.stringify(messages, null, 2));
       }
-
-      let value = getMessageDescriptorValue(valuePath).trim();
-
-      if (key === 'defaultMessage') {
-        try {
-          hash[key] = printICUMessage(value);
-        } catch (parseError) {
-          throw valuePath.buildCodeFrameError(
-            '[React Intl] Message failed to parse. ' +
-            'See: http://formatjs.io/guides/message-syntax/',
-            parseError
-          );
-        }
-      } else {
-        hash[key] = value;
-      }
-
-      return hash;
-    }, {});
-  }
-
-  function storeMessage({ id, description, defaultMessage }, path, state) {
-    const { opts, reactIntl } = state;
-
-    if (!(id && defaultMessage)) {
-      throw path.buildCodeFrameError(
-        '[React Intl] Message Descriptors require an `id` and `defaultMessage`.'
-      );
-    }
-
-    if (reactIntl.messages.has(id)) {
-      let existing = reactIntl.messages.get(id);
-
-      if (description !== existing.description ||
-        defaultMessage !== existing.defaultMessage) {
-
-        throw path.buildCodeFrameError(
-          `[React Intl] Duplicate message id: "${id}", ` +
-          'but the `description` and/or `defaultMessage` are different.'
-        );
-      }
-    }
-
-    if (opts.enforceDescriptions && !description) {
-      throw path.buildCodeFrameError(
-        '[React Intl] Message must have a `description`.'
-      );
-    }
-
-    reactIntl.messages.set(id, { id, description, defaultMessage });
-  }
-
-  function referencesImport(path, mod, importedNames) {
-    if (!(path.isIdentifier() || path.isJSXIdentifier())) {
-      return false;
-    }
-
-    return importedNames.some((name) => path.referencesImport(mod, name));
-  }
-
-  return {
-    visitor: {
-      Program: {
-        enter(path, state) {
-          state.reactIntl = {
-            messages: new Map(),
-          };
-        },
-
-        exit(path, state) {
-          const { file, opts, reactIntl } = state;
-          const { basename, filename }    = file.opts;
-
-          let descriptors = [...reactIntl.messages.values()];
-          file.metadata['react-intl'] = { messages: descriptors };
-
-          if (opts.messagesDir && descriptors.length > 0) {
-            // Make sure the relative path is "absolute" before
-            // joining it with the `messagesDir`.
-            let relativePath = p.join(
-              p.sep,
-              p.relative(process.cwd(), filename)
-            );
-
-            let messagesFilename = p.join(
-              opts.messagesDir,
-              p.dirname(relativePath),
-              basename + '.json'
-            );
-
-            let messagesFile = JSON.stringify(descriptors, null, 2);
-
-            mkdirpSync(p.dirname(messagesFilename));
-            writeFileSync(messagesFilename, messagesFile);
-          }
-        },
-      },
-
-      CallExpression(path, state) {
-        const moduleSourceName = getModuleSourceName(state.opts);
-        const callee = path.get('callee');
-
-        function assertObjectExpression(node) {
-          if (!(node && node.isObjectExpression())) {
-            throw path.buildCodeFrameError(
-              `[React Intl] \`${callee.node.name}()\` must be ` +
-              'called with an object expression with values ' +
-              'that are React Intl Message Descriptors, also ' +
-              'defined as object expressions.'
-            );
-          }
-        }
-
-        function processMessageObject(messageObj) {
-          assertObjectExpression(messageObj);
-
-          let properties = messageObj.get('properties');
-
-          let descriptor = createMessageDescriptor(
-            properties.map((prop) => [
-              prop.get('key'),
-              prop.get('value'),
-            ])
-          );
-
-          if (!descriptor.defaultMessage) {
-            throw path.buildCodeFrameError(
-              '[React Intl] Message is missing a `defaultMessage`.'
-            );
-          }
-
-          storeMessage(descriptor, path, state);
-        }
-
-        if (referencesImport(callee, moduleSourceName, FUNCTION_NAMES)) {
-          let messagesObj = path.get('arguments')[0];
-
-          assertObjectExpression(messagesObj);
-
-          messagesObj.get('properties')
-            .map((prop) => prop.get('value'))
-            .forEach(processMessageObject);
-        }
-      },
     },
-  };
-}
+  },
+});
